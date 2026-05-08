29f2c59e207450bbacb36a8a3f86fdde70529529
Anirudh <141948385+shoppluse@users.noreply.github.com>
2026-05-08 16:41:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ st.set_page_config(page_title="PragyanAI Video Creator", layout="wide")
 if os.path.exists("PragyanAI_Transperent.png"):
     st.image("lord-chaitanya.jpg")
 
-st.title("PragyanAI - Multimedia Merger")
+st.title("Hare Krishna")
 st.markdown("Upload multiple images, specify timing, and add audio from a file or YouTube.")
 
 with st.sidebar:
